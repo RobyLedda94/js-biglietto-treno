a297a4e63241f75b2532f4cf7349686c1b31cfb1
RobyLedda94 <robyledda01@yahoo.it>
2024-06-25 14:38:16 +0200

    [UPD] added prompt for age

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ alert("Javascript calcolo biglietto treno");
 let distanza = parseInt(prompt('Quanti km devi percorrere'))
 console.log(distanza)
 
+
+// CHIEDIAMO L'ETA' AL CLIENTE
+
+let eta = parseInt(prompt('Quanti anni hai?'))
+console.log(eta)
+
+
