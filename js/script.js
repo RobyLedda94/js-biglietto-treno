a8da56de7705e24b2662ab38e5ecc5b16a238877
RobyLedda94 <robyledda01@yahoo.it>
2024-06-25 15:04:58 +0200

    [UPD] added if and else

--- js/script.js
+++ js/script.js
@@ -25,3 +25,14 @@ let costoFinale = distanza * costo;
 console.log(costoFinale)
 
 
+// APPLICAZIONE SCONTO IN BASE ALL'ETA'
+
+let sconto = 0;
+
+if (eta < 18){
+    sconto = 20;
+
+    
+} else if (eta > 65){
+    sconto = 40;
+}
