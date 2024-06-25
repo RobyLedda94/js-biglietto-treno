619256161fa1b7a85319fe8b519ebcb8d03704da
RobyLedda94 <robyledda01@yahoo.it>
2024-06-25 15:39:14 +0200

    [FIX] discount calculation

--- js/script.js
+++ js/script.js
@@ -4,25 +4,25 @@ alert("Javascript calcolo biglietto treno");
 
 // CHIEDIAMO AL CLIENTE QUANTI KILOMETRI DEVE PERCORRERE
 
-let distanza = parseInt(prompt('Quanti km devi percorrere'))
-console.log(distanza)
+let distanza = parseInt(prompt('Quanti km devi percorrere'));
+console.log(distanza);
 
 
 // CHIEDIAMO L'ETA' AL CLIENTE
 
-let eta = parseInt(prompt('Quanti anni hai?'))
-console.log(eta)
+let eta = parseInt(prompt('Quanti anni hai?'));
+console.log(eta);
 
 
 // COSTO DEL BIGLIETTO PER KM
 
 let costo = 0.21;
-console.log(costo)
+console.log(costo);
 
 // CALCOLO COSTO DEL BIGLIETTO
 
 let costoIniziale = distanza * costo;
-console.log(costoIniziale)
+console.log(costoIniziale);
 
 
 // APPLICAZIONE SCONTO IN BASE ALL'ETA'
@@ -30,12 +30,18 @@ console.log(costoIniziale)
 let sconto = 0;
 
 if (eta < 18){
-    sconto = costo - ((costo * 20)/100);
+    sconto = 20;
 
 
 } else if (eta > 65){
-    sconto = costo - ((costo * 40)/100);
+    sconto = 40;
 }
+
+
+// CALCOLO DELLO SCONTO
+
+let prezzoScontato = (costoIniziale * sconto) / 100;
+console.log(prezzoScontato);
 
 
 
