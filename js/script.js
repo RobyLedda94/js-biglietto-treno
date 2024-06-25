d61f67f8328462104895f97cc5272bf197e051ac
RobyLedda94 <robyledda01@yahoo.it>
2024-06-25 14:28:22 +0200

    [UPD] added prompt for distance

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Messaggio alert di prova
 alert("Javascript calcolo biglietto treno");
+
+
+// CHIEDIAMO AL CLIENTE QUANTI KILOMETRI DEVE PERCORRERE
+
+let distanza = parseInt(prompt('Quanti km devi percorrere'))
+console.log(distanza)
+
